fb5567e37e68d1754838784bf1fafae671b7187d
Ramanan-T <ramananthiruchi09@gmail.com>
2025-03-17 23:59:34 +0530

    [Fix] Added logic to extract custom delimiter , sum of numbers between the custom delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,9 +1,14 @@
 class StringCalculator
   def add(input_string)
     return 0 if input_string.empty?
- 
-    number_array = input_string.split(',').map(&:to_i)
- 
+
+    delimiter = ','
+    if input_string.start_with?("//")
+      delimiter_line, input_string = input_string.split("\n", 2)
+      delimiter = delimiter_line.sub("//", "")
+    end
+
+    number_array = input_string.split(delimiter).map(&:to_i) 
     number_array.sum
   end
 end
