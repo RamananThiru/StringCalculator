916dbcee6ddd2d5b202cffac518efa22950604cd
Ramanan-T <ramananthiruchi09@gmail.com>
2025-03-18 00:03:28 +0530

    [fix] [refactor] added logic for raising error when negative number found

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -8,7 +8,20 @@ class StringCalculator
       delimiter = delimiter_line.sub("//", "")
     end
 
-    number_array = input_string.split(delimiter).map(&:to_i) 
+    number_array, negative_number_array = extract_number_arrays(input_string, delimiter)
+    sanitize_input_array(number_array, negative_number_array)
     number_array.sum
+  end
+
+  def extract_number_arrays(input_string, delimiter)
+    number_array = input_string.split(delimiter).map(&:to_i)
+    negative_number_array = number_array.select { |num| num < 0 }
+    [number_array, negative_number_array]
+  end
+
+  def sanitize_input_array(number_array, negative_number_array)
+    if negative_number_array.any?
+      raise "negative numbers not allowed #{negative_number_array.join(',')}"
+    end
   end
 end
