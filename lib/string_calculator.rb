7acd25e0e105eda7c88bcde8d34c47151eb7f136
Ramanan-T <ramananthiruchi09@gmail.com>
2025-03-18 00:11:04 +0530

    [Fix] multiple delimiters with single chars handled

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,11 +2,7 @@ class StringCalculator
   def add(input_string)
     return 0 if input_string.empty?
 
-    delimiter = ','
-    if input_string.start_with?("//")
-      delimiter_line, input_string = input_string.split("\n", 2)
-      delimiter = delimiter_line.sub("//", "")
-    end
+    delimiter, input_string = get_delimiter_and_string(input_string)
 
     number_array, negative_number_array = extract_number_arrays(input_string, delimiter)
     sanitize_input_array(number_array, negative_number_array)
@@ -25,5 +21,31 @@ class StringCalculator
     end
 
     number_array.select! { |num| num <= 1000 }
+  end
+
+  def get_delimiter_and_string(input_string)
+    delimiter = ','
+    string_to_sum = input_string
+
+    if input_string.start_with?("//")
+      delimiter_line, string_to_sum = input_string.split("\n", 2)
+      delimiter = get_delimiter(delimiter_line)
+    end
+    [delimiter, string_to_sum]
+  end
+
+  def get_delimiter(delimiter_line)
+    delimiter = ','
+    if delimiter_line.start_with?("//")
+      delimiter_line = delimiter_line.gsub("//", "") # Remove the "//"
+
+      delimiters = delimiter_line.scan(/\[(.*?)\]/).flatten
+      if delimiters.any?
+        delimiter = Regexp.union(delimiters)
+      else
+        delimiter = delimiter_line
+      end
+    end
+    delimiter
   end
 end
